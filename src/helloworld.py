23bb35141dd7c4cb9022fda43be10bf671eb3d31
Byouree Lim <reelim@gmail.com>
2022-02-12 17:15:54 -0800

    Add my name into comment and code

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,6 +1,6 @@
 
 """
-    This is the hello world program in python
+    This is the hello world program in python by Byouree Lim
 """
 import sys
 # print the version of python you are running, it should be 3.x
@@ -9,3 +9,4 @@ print("Version is: ", sys.version)
 print()
 # print hello world
 print("Hello World")
+print("My name is Byouree Lim")
